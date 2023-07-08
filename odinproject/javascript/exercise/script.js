5a110a75869245f453bb95118fa98b05b6334d74
Dicter Rosas <dicter.r26@ciencias.unam.mx>
2023-07-08 14:39:08 -0600

    Añadiendo archivos que muestran el uso de funciones en js. Responsable: Dicter Rosas

---
+++ odinproject/javascript/exercise/script.js
@@ -0,0 +1,29 @@
+console.log("another way to link javacript code to a html file!")
+
+let name = "john";
+console.log(name);
+let admin = name;
+alert(admin);
+
+let planet = "earth";
+let currentVisitor = "dicter";
+
+console.log(80+20);
+
+console.log((4+6+9)/77);
+
+
+let a = 10;
+console.log(a);
+console.log(9*a);
+
+let b = 7*a;
+console.log(b);
+
+const MAX = 57;
+
+let actual = MAX - 13;
+
+let percentage = actual/MAX;
+
+console.log(percentage);
